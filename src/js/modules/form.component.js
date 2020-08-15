cdd29d03522cfec172a20041bfacbbdd3a6730e9
Jevhenii <alexkorolixinofficial@gmail.com>
2020-08-15 12:42:35 +0300

    add modules: modal, form, validators

---
+++ src/js/modules/form.component.js
@@ -0,0 +1,106 @@
+import Validators from "./validators.component";
+import RequestService from "./../services/request.services";
+
+export default class Form {
+    constructor(form, controls) {
+        this.form = form;
+        this.controls = controls;
+    }
+
+    value() {
+        const value = {};
+        
+        Object.keys(this.controls).forEach(control => {
+            value[control] = this.form[control].value
+        });
+
+        return value;
+    }
+
+    isValid() {
+        let isFormValid = true;
+
+        Object.keys(this.controls).forEach(control => {
+            const validators = this.controls[control];
+
+            let isValid = true;
+
+            validators.forEach(validator => {
+                isValid = validator(this.form[control].value) && isValid;
+            });
+
+            isValid ? _clearError(this.form[control]) :  _setError(this.form[control]);
+
+          
+            isFormValid = isFormValid && isValid;
+        });
+
+        return isFormValid;
+    }
+
+
+    clear() {
+        this.form.reset();
+    }
+
+    destroy() {
+        this.form = null;
+        this.controls = null;
+    }
+}
+
+export function forms(selectorForms, urlSend) {
+    const forms = document.querySelectorAll(selectorForms);
+    const message = {
+        success: "данные успешно отправились",
+        failure: "произошёл сбой отправки",
+        wait: "происходит отправка"
+    }
+    const messageElm = document.createElement("div");
+    forms.forEach(form => {
+        form.addEventListener('submit', _formSubmitHandler);
+    })
+
+    async function _formSubmitHandler(e) {
+        e.preventDefault();
+        messageElm.textContent = message.wait;
+        e.target.appendChild(messageElm);
+
+        let form = new Form(e.target, {
+            user_name:[Validators.required],
+            user_phone:[Validators.required, Validators.minLength(8)]
+        });
+
+        if(form.isValid()) {
+            RequestService.postRequest({...form.value()}, urlSend)
+                .then(response => {
+                    console.log(response);
+                    messageElm.textContent = message.success;
+                    setTimeout(() => {
+                        messageElm.remove();
+                    }, 2000);
+                })
+                .catch(error => {
+                    messageElm.textContent = message.failure;
+                    setTimeout(() => {
+                        messageElm.remove();
+                    }, 5000);
+                });
+            form.clear();
+        }
+    }
+}
+
+function _setError($control) {
+    _clearError($control);
+    const error = '<p class="validation-error">Введите коректное значение</p>'
+    $control.classList.add('invalid');
+    $control.insertAdjacentHTML('afterend', error);
+}
+
+function _clearError($control) {
+    if($control.nextElementSibling && $control.classList.contains('invalid')) {
+        $control.nextElementSibling.remove();
+        $control.classList.remove('invalid');
+    }
+}
---
+++ src/js/modules/modal.component.js
@@ -0,0 +1,98 @@
+
+export default class Modal {
+    constructor(modalSel, buttonOpenSel, classShow='show', formClass='form') {
+       try{
+			this.modal = document.querySelector(modalSel);
+			this.buttonOpenSel = buttonOpenSel;
+			this.modalSel = modalSel;
+			this.buttonOpen = document.querySelector(buttonOpenSel)
+			this.modalClose = this.modal.querySelector('[data-close]');
+			this.classShow = classShow;
+			this.formElm = this.modal.querySelector('.' + formClass);
+ 	   } catch(e) {
+			throw new Error("variables doesn't found");
+	   }
+    }
+
+	modalDelegation(options) {
+		if(options.useButtonsClass) {
+			this.buttonOpen = document.querySelectorAll(this.buttonOpenSel);
+			this.init();
+		}
+
+		if(options.useWrapperDelegationSel) {
+			document.querySelector(options.useWrapperDelegationSel).addEventListener('click', _wrapperDelHandler.bind(this));
+		}
+	}
+
+    init() {
+		if(_checkNodeList.call(this, this.buttonOpen)) {
+			this.buttonOpen.forEach(button => {
+				button.onclick =  _buttonHandler.bind(this);
+			});
+		} else {
+			this.buttonOpen.onclick =  _buttonHandler.bind(this);
+		}
+    }
+
+    show() {
+		this.modal.classList.add(this.classShow);
+		document.body.style.overflow = 'hidden';
+		this.onShow();
+    }
+
+    hide() {
+		this.modal.classList.remove(this.classShow);
+		document.body.style.overflow = '';
+		this.onHide();
+	}
+	
+	destroy() {
+		this.onDestroy();
+	}
+
+
+	showWithDelay(delay) {
+		setTimeout(() => {this.emulatedClick()}, delay);
+	}
+
+	emulatedClick() {
+		typeof this.buttonOpen === 'object' ? this.buttonOpen[0].click() : this.buttonOpen.click();
+	}
+
+	onShow() {}
+
+	onHide() {}
+
+	onDestroy() {}
+}
+
+
+function _buttonHandler(event) {
+	event.preventDefault();
+	this.show();
+	this.modal.onclick = _modalHandler.bind(this);
+}
+function _checkNodeList(array) {
+	return Boolean(array instanceof NodeList);
+}
+function _modalHandler(event) {
+	_modalClose.call(this, event);
+}
+
+function _modalClose(event) {
+	const target = event.target;
+	if(target && (target.closest('[data-close]') === this.modalClose)) {
+		this.hide();
+	}
+
+	if(target && (target === this.modal)) {
+		this.hide();
+	}
+}
+
+function _wrapperDelHandler(e) {
+	if(e.target && e.target.classList.contains(this.buttonOpenSel.slice(1))) {
+		_buttonHandler.call(this, e);
+	}
+}
